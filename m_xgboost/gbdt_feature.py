e88161d2323a6ba160bf663fcd3d156898421f70
dwt0317 <dwt0317@163.com>
2017-04-28 17:08:35 +0800

    gbdt生成特征完成

--- m_xgboost/gbdt_feature.py
+++ m_xgboost/gbdt_feature.py
@@ -5,10 +5,11 @@ from sklearn import metrics   #Additional scklearn functions
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import GridSearchCV   #Perforing grid search
 from xgboost.sklearn import XGBClassifier
-
+import cPickle as pickle
 import Constants
 import xgboost as xgb
-
+import pandas as pd
+from pandas import DataFrame
 
 # tp, fn, fp, tn
 def get_metric(test_y, train_pred):
@@ -39,7 +40,7 @@ def train_model():
 
     train_data = load_svmlight_file(train_x)
 
-    rounds = 20
+    rounds = 30
     classifier = XGBClassifier(learning_rate=0.1, n_estimators=rounds, max_depth=3,
                                min_child_weight=1, gamma=0, subsample=0.8,
                                objective='binary:logistic', nthread=2)
@@ -62,7 +63,7 @@ def train_model():
         watchlist = [(train_set, 'train'), (validation_set, 'eval')]
         params = {"objective": 'binary:logistic',
                   "booster": "gbtree",
-                  'eval_metric': 'auc',
+                  'eval_metric': 'error',
                   "eta": 0.1,
                   "max_depth": 3,
                   'silent': 0,
@@ -70,7 +71,8 @@ def train_model():
                   'subsample': 0.8,
                   'gamma': 0,
                   'early_stopping_rounds': 10,
-                  'nthread': 2
+                  'nthread': 2,
+                  'max_leaf_nodes': 20
                   }
         print "Training model..."
         xgb_model = xgb.train(params, train_set, rounds, watchlist, verbose_eval=True)
@@ -78,18 +80,37 @@ def train_model():
         print train_pred
         auc_test = metrics.roc_auc_score(test_y, train_pred)
         print auc_test
+        test_ind = xgb_model.predict(xgb.DMatrix(test_x), ntree_limit=xgb_model.best_ntree_limit, pred_leaf=True)
+        train_ind = xgb_model.predict(xgb.DMatrix(train_x), ntree_limit=xgb_model.best_ntree_limit, pred_leaf=True)
+        pickle.dump(test_ind, open(Constants.dir_path + "sample\\features\\gbdt_features\\test_2.idx", "wb"))
+        pickle.dump(train_ind, open(Constants.dir_path + "sample\\features\\gbdt_features\\training_2.idx", "wb"))
+        print test_ind.shape, train_ind.shape
 
-    # test_ind = xgb_model.predict(xgb.DMatrix(test_x), ntree_limit=xgb_model.best_ntree_limit, pred_leaf=True)
-    # train_ind = xgb_model.predict(xgb.DMatrix(train_x), ntree_limit=xgb_model.best_ntree_limit, pred_leaf=True)
-    #
-    # print test_ind.shape, train_ind.shape
-    # for i in range(30):
-    #     print test_ind[i]
-    # # pickle.dump(test_ind, open(Constants.dir_path + "sample\\features\\m_gbdt\\test.idx", "wb"))
-    # # pickle.dump(train_ind, open(Constants.dir_path + "sample\\features\\m_gbdt\\training.idx", "wb"))
-    # print "All done"
 
+
+def onehot_feature():
+    print "load_data"
+    onehot = []
+    print "transform"
+    gbdt_feature = pickle.load(open(Constants.dir_path + "sample\\features\\gbdt_features\\training_2.idx", "rb"))
+
+    for line in gbdt_feature:
+        temp_onehot = []
+        i = 0
+        for item in line:
+            temp_onehot.append(int(item) + i*20 - 1)
+            i += 1
+        onehot.append(temp_onehot)
+
+    pickle.dump(onehot, open(Constants.dir_path + "sample\\features\\gbdt_features\\train.onehot.dict", "wb"))
+
+
+def test_read():
+    gbdt_feature = pickle.load(open(Constants.dir_path + "sample\\features\\gbdt_features\\train.onehot.dict", "rb"))
+    for i in range(3):
+        print gbdt_feature[i]
 
 if __name__ == '__main__':
-    train_model()
+    # train_model()
+    onehot_feature()
 
